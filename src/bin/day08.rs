830641778e4192fe9a496862d17e33b0e77f8771
Simon A. Berger <simberger@gmail.com>
2025-12-08 22:43:16 +0100

    don't use nalgebra

--- src/bin/day08.rs
+++ src/bin/day08.rs
@@ -2,26 +2,34 @@ use std::collections::HashSet;
 
 type Result<T> = anyhow::Result<T>;
 
-type Vec3 = nalgebra::Vector3<f32>;
+// type Vec3 = nalgebra::Vector3<f32>;
 
 fn main() -> Result<()> {
     let s = std::fs::read_to_string("input/day08.txt")?;
 
-    let vs: Vec<Vec3> = s
+    let vs: Vec<(i32, i32, i32)> = s
         .lines()
         .map(|line| {
             let mut cs = line.split(',');
             let x = cs.next().unwrap();
             let y = cs.next().unwrap();
             let z = cs.next().unwrap();
-            Vec3::new(x.parse().unwrap(), y.parse().unwrap(), z.parse().unwrap())
+            // Vec3::new
+            (x.parse().unwrap(), y.parse().unwrap(), z.parse().unwrap())
         })
         .collect();
 
     let mut dists = Vec::new();
     for i in 0..vs.len() {
         for j in 0..i {
-            dists.push((vs[i].metric_distance(&vs[j]) as i32, i, j));
+            // dists.push((vs[i].metric_distance(&vs[j]) as i32, i, j));
+            let (xi, yi, zi) = vs[i];
+            let (xj, yj, zj) = vs[j];
+            let dx = (xi - xj) as f32;
+            let dy = (yi - yj) as f32;
+            let dz = (zi - zj) as f32;
+            let d = (dx * dx) + (dy * dy) + (dz * dz);
+            dists.push((d.sqrt() as i32, i, j));
         }
     }
 
@@ -64,7 +72,7 @@ fn main() -> Result<()> {
             println!("res1: {res}");
         }
         if graphs.len() == 1 && graphs[0].len() == vs.len() && !res2_done {
-            let res2 = vs[*i][0] * vs[*j][0];
+            let res2 = vs[*i].0 * vs[*j].0;
             res2_done = true;
             println!("res2: {res2}");
         }
